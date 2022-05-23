ecbca32df48dea79f2fb081ae30ab7b663b0d075
Timur <2001rich04@gmail.com>
2022-05-23 10:18:14 +0300

    Add EmployeeRequest_Delete

---
+++ HRproject/HRproject.DB/dbo/Stored Procedures/EmployeeRequest_Delete.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE dbo.EmployeeRequest_Delete
+@Id int,
+@ProjectId,
+@Quantity,
+@IsDeleted
+AS
+BEGIN
+UPDATE dbo.Employee
+SET ProjectId=@ProjectId,
+Quantity=@Quantity,
+IsDeleted=@IsDeleted
+WHERE Id = @Id
+END
