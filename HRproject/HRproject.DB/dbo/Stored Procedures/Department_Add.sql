07183f57a13919939e4b6d5abc4ca61e37e71fc6
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-05-23 15:46:35 +0300

    Add Department_Add

---
+++ HRproject/HRproject.DB/dbo/Stored Procedures/Department_Add.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[Department_Add]
+	@id int,
+	@Name varchar(50),
+	@Description text,
+	@IsDeleted bit
+AS
+BEGIN
+INSERT INTO dbo.Department(
+	[Name],
+	[Description],
+	IsDeleted
+)
+VALUES(
+	@Name,
+	@Description,
+	@IsDeleted
+)
+
+SELECT
+	[Name]=@Name,
+	[Description]=@Description,
+	IsDeleted=@IsDeleted
+FROM dbo.Department
+WHERE id=@id
+END
+
