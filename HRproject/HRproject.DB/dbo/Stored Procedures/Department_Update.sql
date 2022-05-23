ca05ab3802ec41f9f108192647b4469eec47802f
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-05-23 16:17:10 +0300

    Add Department_Update

---
+++ HRproject/HRproject.DB/dbo/Stored Procedures/Department_Update.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[Department_Update]
+	@id int,
+	@Name varchar(50),
+	@Description text,
+	@isDeleted bit
+AS
+BEGIN
+
+UPDATE dbo.Department
+SET [Name]=@Name,
+	[Description]=@Description,
+	isDeleted=@isDeleted
+WHERE id=@id
+
+END
