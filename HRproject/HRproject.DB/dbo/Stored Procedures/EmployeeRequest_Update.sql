46298b3de7cf45575a5e65eb25d0667b7e142a25
Timur <2001rich04@gmail.com>
2022-05-23 10:19:52 +0300

    Add EmployeeRequest_Update

---
+++ HRproject/HRproject.DB/dbo/Stored Procedures/EmployeeRequest_Update.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE dbo.EmployeeRequest_Update
+@Id int,
+@ProjectId,
+@Quantity,
+@IsDeleted
+AS
+BEGIN
+UPDATE dbo.Employee
+SET ProjectId=@ProjectId,
+Quantity=@Quantity
+IsDeleted=@IsDeleted
+WHERE Id = @Id
+END
