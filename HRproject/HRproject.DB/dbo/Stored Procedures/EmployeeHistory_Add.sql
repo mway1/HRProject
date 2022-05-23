746219a916329a4b1a4ccf2c49fa749157b6007d
Daria Shadrina <darinashadrina@yandex.ru>
2022-05-23 15:54:32 +0300

    Add EmployeeHistory_Add

---
+++ HRproject/HRproject.DB/dbo/Stored Procedures/EmployeeHistory_Add.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[EmployeeHistory_Add]
+	@id int,
+	@EmployeeId int,
+	@Date timestamp,
+	@StatusId int,
+	@isDeleted bit
+
+AS
+BEGIN
+INSERT INTO dbo.EmployeeHistory(
+EmployeeId,
+[Date],
+StatusId,
+isDeleted
+)
+VALUES(
+
+)
+	SELECT @param1, @param2
+RETURN 0
