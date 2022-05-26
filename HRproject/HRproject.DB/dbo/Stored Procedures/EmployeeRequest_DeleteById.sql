ea67a9b1aa1ad3572c184a41cfaf7bafdf122f74
Timur <2001rich04@gmail.com>
2022-05-26 12:49:03 +0300

    Fix DeleteById
    EmployeeRequest

--- HRproject/HRproject.DB/dbo/Stored Procedures/EmployeeRequest_DeleteById.sql
+++ HRproject/HRproject.DB/dbo/Stored Procedures/EmployeeRequest_DeleteById.sql
@@ -1,13 +1,12 @@
-﻿CREATE PROCEDURE dbo.EmployeeRequest_Delete
+﻿CREATE PROCEDURE dbo.EmployeeRequest_DeleteById
 @Id int,
 @ProjectId int,
-@Quantity int,
-@IsDeleted bit
+@Quantity int
 AS
 BEGIN
 UPDATE dbo.EmployeeRequest
 SET ProjectId=@ProjectId,
 Quantity=@Quantity,
-IsDeleted=@IsDeleted
+IsDeleted= 1
 WHERE Id = @Id
 END
