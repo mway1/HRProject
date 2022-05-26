3fba12e63f1cbc5532d0e691ecb4af3218c517db
Maxim Kuzmenko <mway2914@yandex.ru>
2022-05-27 00:31:22 +0300

    Create Employee_Project_add

---
+++ HRproject/HRproject.DB/dbo/Stored Procedures/Employee_Project_Add.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[Employee_Project_Add]
+		@id int,
+		@EmployeeId int,
+		@ProjectId int
+AS
+BEGIN
+INSERT INTO dbo.Employee_Project(
+        EmployeeId,
+		ProjectId)
+VALUES(
+		@EmployeeId,
+		@ProjectId)
+SET @id=SCOPE_IDENTITY()
+
+SELECT
+		EmployeeId=@EmployeeId,
+		ProjectId=@ProjectId
+FROM dbo.Employee_Project
+WHERE id=@id
+END
