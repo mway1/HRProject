680482242e73ddaa1ea63fa75b1e10ecd703ebb5
Timur <2001rich04@gmail.com>
2022-05-26 13:25:22 +0300

    fix

--- HRproject/HRproject.DB/dbo/Stored Procedures/EmployeeRequest_GetAllInfoById.sql
+++ HRproject/HRproject.DB/dbo/Stored Procedures/EmployeeRequest_GetAllInfoById.sql
@@ -4,13 +4,12 @@ AS
 BEGIN
 SELECT Pr.[Name], Po.[Name], LOP.[Name], S.[name], LOS.[Name], E.[Quantity] 
 FROM EmployeeRequest as E
-JOIN Project as Pr ON (Pr.id = E.ProjectId)
-JOIN EmployeeRequest_Skill as ES on (E.id = ES.EmployeeRequestId)
-JOIN Skill as S on (S.id = ES.SkillId)
-JOIN LevelOfSkill as LOS on (LOS.id = S.LevelOfSKillId)
-JOIN EmployeeRequest_Position as EP on (E.id = EP.EmployeeRequestId)
-JOIN Position as Po on (Po.id = EP.PositionId)
-JOIN Position_LevelOfPosition as PL on (PL.PositionId = Po.id)
-JOIN LevelOfPOsition as LOP on (LOP.id = PL.LevelOfPositionId)
+LEFT JOIN Project as Pr ON (E.ProjectId = Pr.id)
+LEFT JOIN EmployeeRequest_Skill as ES on (E.id = ES.EmployeeRequestId)
+LEFT JOIN Skill as S on (ES.SkillId = S.id)
+LEFT JOIN EmployeeRequest_Position as EP on (E.id = EP.EmployeeRequestId)
+LEFT JOIN Position as Po on (Po.id = EP.PositionId)
+LEFT JOIN Position_LevelOfPosition as PL on (Po.id = PL.PositionId)
+LEFT JOIN LevelOfPOsition as LOP on (LOP.id = PL.LevelOfPositionId)
 WHERE E.id = @Id
 END
