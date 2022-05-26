aae09d34f9aa55c4b7d79fc3bbb9f92d55476ef1
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-05-26 19:27:58 +0300

    Create class DepartmentDTO

---
+++ HRproject/HRProject.DAL/DTOs/DepartmentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.DAL.DTOs
+{
+    public class DepartmentDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
