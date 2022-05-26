e5f9f67b8f704c8c64f2030ea7d78464fbf914cf
Maxim Kuzmenko <mway2914@yandex.ru>
2022-05-27 00:58:04 +0300

    Add Employee_Project_GetAll

---
+++ HRproject/HRproject.DB/dbo/Stored Procedures/Employee_Project_GetAll.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Employee_Project_GetAll]
+	AS
+BEGIN
+
+	SELECT EP.Id,E.FirstName,E.LastName,P.Name
+	FROM dbo.Employee_Project as EP
+	LEFT JOIN dbo.Employee as E on (E.id=EP.EmployeeId)
+	LEFT JOIN dbo.Project as P on (P.id=EP.ProjectId)
+
+END
