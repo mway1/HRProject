9e64196669497fd82147cf5a7c5d071789b026be
Timur <2001rich04@gmail.com>
2022-05-26 14:14:22 +0300

    Add DTO fro EmployeeRequestAllInfo

---
+++ HRproject/HRProject.DAL/DTOs/EmplooyeeRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.DAL.DTOs
+{
+    public class EmplooyeeRequestAllInfoDTO
+    {
+        public string ProjectName { get; set; }
+        public string PositionName { get; set; }
+        public string LevelOfPositionName { get; set; }
+        public string SkillName { get; set; }
+        public int LevelOfSkill { get; set; }
+        public int Quantity { get; set; }
+    }
+}
