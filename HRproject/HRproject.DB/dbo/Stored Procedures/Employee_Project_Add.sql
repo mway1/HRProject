e0263d3aa94bc15f88eb1f4836bcd62e75683cf9
Maxim Kuzmenko <mway2914@yandex.ru>
2022-05-27 00:51:27 +0300

    Fixing

--- HRproject/HRproject.DB/dbo/Stored Procedures/Employee_Project_Add.sql
+++ HRproject/HRproject.DB/dbo/Stored Procedures/Employee_Project_Add.sql
@@ -7,14 +7,12 @@ BEGIN
 INSERT INTO dbo.Employee_Project(
         EmployeeId,
 		ProjectId)
-VALUES(
-		@EmployeeId,
-		@ProjectId)
+VALUES( @EmployeeId,
+        @ProjectId)
 SET @id=SCOPE_IDENTITY()
 
-SELECT
-		EmployeeId=@EmployeeId,
-		ProjectId=@ProjectId
+SELECT EmployeeId=@EmployeeId,
+       ProcetId=@ProjectId
 FROM dbo.Employee_Project
 WHERE id=@id
 END
