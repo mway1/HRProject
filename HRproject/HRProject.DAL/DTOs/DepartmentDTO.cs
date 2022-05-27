23376e56a78cfa66e8fbc036d7ccfa63fd8df684
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-05-27 07:51:57 +0300

    Create class DepartmentManager

--- HRproject/HRProject.DAL/DTOs/DepartmentDTO.cs
+++ HRproject/HRProject.DAL/DTOs/DepartmentDTO.cs
@@ -10,7 +10,8 @@ namespace HRProject.DAL.DTOs
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } 
+        public bool IsDeleted { get; set; }
 
 
     }
---
+++ HRproject/HRProject.DAL/DepartmentManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using HRProject.DAL.DTOs;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace HRProject.DAL
+{
+    public class DepartmentManager
+    {
+        public string _connectionString = @"Server=.\SQLEXPRESS;Database=HRProject.DB;Trusted_Connection=True;";
+        public List<DepartmentDTO> DpeartmentGetById() 
+        {
+            using var connection = new SqlConnection(_connectionString); ;
+        }
+    }
+}
