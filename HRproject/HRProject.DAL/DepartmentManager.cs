de95be37be965d9fccdddd6ddb05ffc254e353ed
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-05-27 08:26:49 +0300

    Add DepartmentGetAll method in DAL

--- HRproject/HRProject.DAL/DepartmentManager.cs
+++ HRproject/HRProject.DAL/DepartmentManager.cs
@@ -8,9 +8,30 @@ namespace HRProject.DAL
     public class DepartmentManager
     {
         public string _connectionString = @"Server=.\SQLEXPRESS;Database=HRProject.DB;Trusted_Connection=True;";
-        public List<DepartmentDTO> DpeartmentGetById() 
+        public DepartmentDTO DpeartmentGetById(int id) 
         {
-            using var connection = new SqlConnection(_connectionString); ;
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<DepartmentDTO>(
+                    "dbo.Department_GetById", 
+                    param: new {id=id},
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public DepartmentDTO DepartmentGetAll()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<DepartmentDTO>(
+                    "dbo.Department_GetAll",
+                    commandType: System.Data.CommandType.StoredProcedure)
+                    .ToList();
+            }
         }
     }
 }
