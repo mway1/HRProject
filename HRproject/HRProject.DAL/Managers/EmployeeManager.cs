8a23c895c9a1090202ed42eac539cfd0f030a0c2
Maxim Kuzmenko <mway2914@yandex.ru>
2022-05-28 20:53:32 +0300

    add EmployeeManager and add GetAllEmployee

---
+++ HRproject/HRProject.DAL/Managers/EmployeeManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using HRProject.DAL.DTOs;
+
+namespace HRProject.DAL.Managers
+{
+    public class EmployeeManager
+    {
+        public List<EmployeeDTO> GetAllEmployee()
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<EmployeeDTO>(
+                    StoredProcedures.Employee_GetAll,
+                    commandType: System.Data.CommandType.StoredProcedure)
+                    .ToList();
+                )
+            }
+        }
+    }
+}
