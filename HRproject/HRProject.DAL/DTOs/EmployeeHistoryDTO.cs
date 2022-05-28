bcc794c6f04ca8e633493132ed20ae4da7327cf4
Daria Shadrina <darinashadrina@yandex.ru>
2022-05-28 14:19:26 +0300

    Add override in DTO

--- HRproject/HRProject.DAL/DTOs/EmployeeHistoryDTO.cs
+++ HRproject/HRProject.DAL/DTOs/EmployeeHistoryDTO.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace HRProject.DAL.DTOs
 {
-    internal class EmployeeHistoryDTO
+    public  class EmployeeHistoryDTO
     {
+        public int id { get; set; }
+        public int EmployeeId { get; set; }
+        public string Date { get; set; }
+        public string StatusId { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public override string ToString()
+        {
+            return $"id={id} EmployeeId={EmployeeId} Date ={Date} StatusId={StatusId} IsDeleted={IsDeleted}"  ;
+        }
     }
 }
