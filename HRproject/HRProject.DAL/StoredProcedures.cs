912da5828c1bb9f25b54ccfa0d84468bb2eeece5
Daria Shadrina <darinashadrina@yandex.ru>
2022-05-28 16:21:10 +0300

    Create DAL for EmployeeHistory

---
+++ HRproject/HRProject.DAL/StoredProcedures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.DAL
+{
+    public class StoredProcedures
+    {
+        public const string EmployeeHistory_GetAll = "EmployeeHistory_GetAll";
+        public const string EmployeeHistory_GetById = "EmployeeHistory_GetById";
+        public const string EmployeeHistory_Add = "EmployeeHistory_Add";
+        public const string EmployeeHistory_Update = "EmployeeHistory_Update";
+        public const string EmployeeHistory_Delete = "EmployeeHistory_Delete";
+
+    }
+}
