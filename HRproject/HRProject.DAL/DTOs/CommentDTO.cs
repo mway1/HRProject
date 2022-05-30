e70d9a40fd01b7d873a6ffc983b17677d77c2998
Maxim Kuzmenko <mway2914@yandex.ru>
2022-05-30 22:10:58 +0300

    end CommentDTO

--- HRproject/HRProject.DAL/DTOs/CommentDTO.cs
+++ HRproject/HRProject.DAL/DTOs/CommentDTO.cs
@@ -8,5 +8,14 @@ namespace HRProject.DAL.DTOs
 {
     public class CommentDTO
     {
+        public int id { get; set; }
+        public string Description { get; set; }
+        public int EmployeeId { get; set; }
+        public bool isDeleted { get; set; }
+        public override string ToString()
+        {
+            return $"id={id} Description={Description} EmployeeId={EmployeeId} isDeleted={isDeleted}";
+        }
+
     }
 }
