dd992990fe95ea7c7708e4f83a28e1950688497b
Eugen <phlosof@gmail.com>
2022-05-30 11:46:45 +0300

    add PositionDTO

---
+++ HRproject/HRProject.DAL/DTOs/PositionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.DAL.DTOs
+{
+    internal class PositionDTO
+    {
+        public int id { get; set; }
+        public string Position { get; set; }
+        public string LevelOfPosition { get; set; }
+    }
+}
