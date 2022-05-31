7736dc9f51d5c49dc39c448086157f40387a36f0
Timur <2001rich04@gmail.com>
2022-05-31 15:02:28 +0300

    Rewrite EmployeeRequestAllInfoDto

--- HRproject/HRProject.DAL/DTOs/EmplooyeeRequestAllInfoDTO.cs
+++ HRproject/HRProject.DAL/DTOs/EmplooyeeRequestAllInfoDTO.cs
@@ -9,7 +9,7 @@ namespace HRProject.DAL.DTOs
     public class EmplooyeeRequestAllInfoDTO
     {
         public int id { get; set; }
-        public int Quantity { get; set; }
+        public int? Quantity { get; set; }
         public string ProjectName { get; set; }
         public List<string> PositionName { get; set; }
         public List<string> PositionLevel { get; set; }
