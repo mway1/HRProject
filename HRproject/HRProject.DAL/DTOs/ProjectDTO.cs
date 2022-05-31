17d9e5739b5c97a1c16dcdb68823bade556479ee
Timur <2001rich04@gmail.com>
2022-05-31 15:16:35 +0300

    Add ProjectDTO

---
+++ HRproject/HRProject.DAL/DTOs/ProjectDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.DAL.DTOs
+{
+    public class ProjectDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? IsDeleted { get; set; }
+    }
+}
