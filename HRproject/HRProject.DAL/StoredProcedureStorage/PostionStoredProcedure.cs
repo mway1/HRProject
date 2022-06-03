604a27852af1a63185865dbaedabe864378ca58f
Eugen <phlosof@gmail.com>
2022-06-03 13:57:44 +0300

    Add PostionStoredProcedure in StoredProcedureStorage

---
+++ HRproject/HRProject.DAL/StoredProcedureStorage/PostionStoredProcedure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.DAL.StoredProcedureStorage
+{
+    public class PostionStoredProcedure
+    {
+        public const string Position_Add = "Position_Add";
+        public const string Position_DeleteById = "Position_DeleteById";
+        public const string Position_GetAll = "Position_GetAll";
+        public const string Position_GetById = "Position_GetById";
+        public const string Position_Update = "Position_Update";
+    }
+}
