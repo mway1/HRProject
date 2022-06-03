1230143aecf0e7c71c57704d1590b1265c4e92db
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-06-03 07:35:10 +0300

    Add class DepartmentService

---
+++ HRproject/HRProject.BLL/Service/DepartmentService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HRProject.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.BLL.Service
+{
+    public class DepartmentService
+    {
+        private readonly Mapper _mapper;
+        private readonly DepartmentManager _departmentManager;
+
+        public DepartmentService(Mapper mapper, DepartmentManager departmentManager)
+        {
+            _mapper = MapperConfigStorage.GetInstance();
+            _departmentManager = departmentManager;
+        }
+
+    }
+}
