0bdc1419c0e3628c7e6721dd8f40b15947d1575f
Eugen <phlosof@gmail.com>
2022-06-06 01:30:31 +0300

    fill PositionManager and rename Position_DeleteById to Position_Delete

--- HRproject/HRProject.DAL/Managers/PositionManager.cs
+++ HRproject/HRProject.DAL/Managers/PositionManager.cs
@@ -6,6 +6,31 @@ namespace HRProject.DAL
 {
     public class PositionManager
     {
+        public PositionDTO GetPositonById(int positionId)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<PositionDTO>(
+                    PostionStoredProcedure.Position_GetById,
+                    param: new { id = positionId },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+        public List<PositionDTO> GetAllPositon(int positionId)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<PositionDTO>(
+                    PostionStoredProcedure.Position_GetAll,
+                    param: new { id = positionId },
+                    commandType: System.Data.CommandType.StoredProcedure)
+                    .ToList();
+            }
+        }
         public void AddPosition(PositionDTO positionDTO)
         {
             using (var connection = new SqlConnection(ServerSettings._connectionString))
@@ -17,10 +42,45 @@ namespace HRProject.DAL
                     param: new
                     {
                         Name = positionDTO.Name,
-                        isDeleted = positionDTO.IsDeleted
+                        LevelOfPositionId = positionDTO.PositionLevel.Id
+
                     },
                     commandType: System.Data.CommandType.StoredProcedure);
             }
+        }
+
+        public void UpdatePosition(int positionId, PositionDTO positionDTO)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle(
+                    PostionStoredProcedure.Position_Update,
+                    param: new
+                    {
+                        Id = positionId,
+                        Name = positionDTO.Name,
+                        LevelOfPositionId = positionDTO.PositionLevel.Id
+
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public void DeletePosition(int positionId)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle(
+                    PostionStoredProcedure.Position_Delete,
+                    param: new
+                    {  Id = positionId  },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+
         }
 
     }
--- HRproject/HRProject.DAL/StoredProcedureStorage/PostionStoredProcedure.cs
+++ HRproject/HRProject.DAL/StoredProcedureStorage/PostionStoredProcedure.cs
@@ -9,7 +9,7 @@ namespace HRProject.DAL
     public class PostionStoredProcedure
     {
         public const string Position_Add = "Position_Add";
-        public const string Position_DeleteById = "Position_DeleteById";
+        public const string Position_Delete = "Position_Delete";
         public const string Position_GetAll = "Position_GetAll";
         public const string Position_GetById = "Position_GetById";
         public const string Position_Update = "Position_Update";
