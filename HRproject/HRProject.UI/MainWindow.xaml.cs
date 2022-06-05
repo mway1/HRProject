fa24144639d764b4e028881fc45e85ef34548771
Timur <2001rich04@gmail.com>
2022-06-06 00:43:58 +0300

    fix after merge

--- HRproject/HRProject.UI/MainWindow.xaml.cs
+++ HRproject/HRProject.UI/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows;
 using System.Collections.ObjectModel;
 using System;
 using System.Collections.Generic;
+using System.Windows.Controls;
+using HRProject.BLL.OutputModels;
 
 namespace HRProject.UI
 {
@@ -13,8 +15,8 @@ namespace HRProject.UI
     public partial class MainWindow : Window
     {
 
-        private Controller controller = new Controller();
         private ObservableCollection<ProjectOutputModel> Projects = new ObservableCollection<ProjectOutputModel>();
+        private Controller _controller;
 
         public MainWindow()
         {
@@ -27,7 +29,9 @@ namespace HRProject.UI
             ComboBoxProject.ItemsSource = ProjectTypes.Types;
             ListBoxProjects.ItemsSource = Projects;
 
-            LoadProjectList(controller.GetAllProjects());
+            LoadProjectList(_controller.GetAllProjects());
+
+            _controller = new Controller();
         }
 
         private void LoadProjectList(List<ProjectOutputModel> projects)
@@ -39,29 +43,17 @@ namespace HRProject.UI
             }
         }
 
-        private void Window_Initialized(object sender, EventArgs e)
-        {
-            _controller = new Controller();
-        }
-
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var selectedProject = (ProjectModel)ListBoxProjects.SelectedItem;
             var choosenEmployeeRequests = _controller.GetEmployeeRequestAllInfoByProjectId(selectedProject.Id);
             ListBoxEmployeeRequests.ItemsSource = choosenEmployeeRequests;
         }
-    }
-}
         private void ComboBoxProject_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             LoadProjectList(
-                controller.GetAllProjects(ComboBoxProject.SelectedItem.ToString()!)
+                _controller.GetAllProjects(ComboBoxProject.SelectedItem.ToString()!)
             );
-        }
-
-        private void ListBoxProjects_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
