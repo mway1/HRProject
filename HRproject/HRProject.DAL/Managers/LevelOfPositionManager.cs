3530d07dcb9f5133cf496540b92302fed19c38f6
Maxim Kuzmenko <mway2914@yandex.ru>
2022-06-05 16:04:21 +0300

    create LevelOfPositionManager and add GetAllLevelOfPosition

---
+++ HRproject/HRProject.DAL/Managers/LevelOfPositionManager.cs
@@ -0,0 +1,29 @@
+﻿using HRProject.DAL.DTOs;
+using HRProject.DAL.StoredProcedureStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace HRProject.DAL.Managers
+{
+    public class LevelOfPositionManager
+    {
+        public List<LevelOfPositionDTO> GetAllLevelOfPosition()
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<LevelOfPositionDTO>(
+                    LevelOfPositionStoredProcedures.LevelOfPosition_Get,
+                    commandType: System.Data.CommandType.StoredProcedure)
+                    .ToList();
+            }
+        }
+
+    }
+}
