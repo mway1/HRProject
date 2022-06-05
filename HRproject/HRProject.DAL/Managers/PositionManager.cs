4ef1a92d58d87608338082f968889aabfdb45286
Eugen <phlosof@gmail.com>
2022-06-06 00:41:21 +0300

    add PositionManager

---
+++ HRproject/HRProject.DAL/Managers/PositionManager.cs
@@ -0,0 +1,27 @@
+﻿using HRProject.DAL.DTOs;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace HRProject.DAL
+{
+    public class PositionManager
+    {
+        public void AddPosition(PositionDTO positionDTO)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle(
+                    PostionStoredProcedure.Position_Add,
+                    param: new
+                    {
+                        Name = positionDTO.Name,
+                        isDeleted = positionDTO.IsDeleted
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+    }
+}
--- HRproject/HRProject.DAL/StoredProcedureStorage/PostionStoredProcedure.cs
+++ HRproject/HRProject.DAL/StoredProcedureStorage/PostionStoredProcedure.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HRProject.DAL.StoredProcedureStorage
+namespace HRProject.DAL
 {
     public class PostionStoredProcedure
     {
