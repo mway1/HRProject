795af0da90c078928b254b8b46c7d058003a4458
Timur <2001rich04@gmail.com>
2022-06-07 14:16:50 +0300

    fix tables

--- HRproject/HRproject.DB/dbo/Tables/Skill.sql
+++ HRproject/HRproject.DB/dbo/Tables/Skill.sql
@@ -1,9 +1,7 @@
 ﻿CREATE TABLE [dbo].[Skill] (
     [id]             INT          IDENTITY (1, 1) NOT NULL,
     [Name]           VARCHAR (50) NOT NULL,
-    [LevelOfSkillId] INT          NOT NULL,
     [isDeleted]      BIT          NULL,
     PRIMARY KEY CLUSTERED ([id] ASC),
-    FOREIGN KEY ([LevelOfSkillId]) REFERENCES [dbo].[LevelOfSkill] ([id])
 );
 
