d6757de7f991c39d4a466dab870e703604c1786f
Maxim Kuzmenko <mway2914@yandex.ru>
2022-06-09 01:47:34 +0300

    done output Employee All info

--- HRproject/HRProject.UI/MainWindow.xaml.cs
+++ HRproject/HRProject.UI/MainWindow.xaml.cs
@@ -24,13 +24,6 @@ namespace HRProject.UI
         {
             this.Initialized += Window_Initialized;
             InitializeComponent();
-            var chooseEmployeeAllInfo = _controller.GetEmployeeById(_employeeId);
-            TextBox_FirstName.Text = chooseEmployeeAllInfo.FirstName;
-            TextBox_LastName.Text = chooseEmployeeAllInfo.LastName;
-            TextBox_SecondName.Text = chooseEmployeeAllInfo.SecondName;
-            TextBox_BirthDate.Text = chooseEmployeeAllInfo.BirthDate.ToString();
-            TextBox_Email.Text = chooseEmployeeAllInfo.Email;
-            TextBox_PhoneNumber.Text = chooseEmployeeAllInfo.PhoneNumber.ToString();
         }
 
         public void Window_Initialized(object? sender, EventArgs e)
@@ -74,6 +67,19 @@ namespace HRProject.UI
 
         private void Employees_General_TabIItem_Loaded(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void Employees_General_TabIItem_Initialized(object sender, EventArgs e)
+        {
+            var chooseEmployeeAllInfo = _controller.GetEmployeeById(_employeeId);
+            TextBox_FirstName.Text = chooseEmployeeAllInfo.FirstName;
+            TextBox_LastName.Text = chooseEmployeeAllInfo.LastName;
+            TextBox_SecondName.Text = chooseEmployeeAllInfo.SecondName;
+            TextBox_BirthDate.Text = chooseEmployeeAllInfo.BirthDate.ToString();
+            TextBox_Email.Text = chooseEmployeeAllInfo.Email;
+            TextBox_PhoneNumber.Text = chooseEmployeeAllInfo.PhoneNumber.ToString();
+            ComboBox_Status_Tab1.SelectedIndex = chooseEmployeeAllInfo.StatusId;
+
         }
     }
 }
