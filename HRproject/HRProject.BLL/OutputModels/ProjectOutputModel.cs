915d77d811a9c3f13bf2d91c22fe7a96b6765b5d
Timur <2001rich04@gmail.com>
2022-06-08 20:41:34 +0300

    Add id to ProjectOutputModel

--- HRproject/HRProject.BLL/OutputModels/ProjectOutputModel.cs
+++ HRproject/HRProject.BLL/OutputModels/ProjectOutputModel.cs
@@ -4,6 +4,7 @@ namespace HRProject.BLL.Models
 {
     public class ProjectOutputModel
     {
+        public int id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public override string ToString()
