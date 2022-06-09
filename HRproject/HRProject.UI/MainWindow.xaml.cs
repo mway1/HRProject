b23dcf6ad18be2ca24410fc260b9768dccaea3ff
Maxim Kuzmenko <mway2914@yandex.ru>
2022-06-09 22:17:31 +0300

    some fix

--- HRproject/HRProject.UI/MainWindow.xaml.cs
+++ HRproject/HRProject.UI/MainWindow.xaml.cs
@@ -120,7 +120,7 @@ namespace HRProject.UI
             {
                 var chooseEmployeeAllInfo = _controller.GetEmployeeById(selectedEmployee.id);
                 TextBox_FirstName.Text = chooseEmployeeAllInfo.FirstName;
-                //TextBox_LastName.Text = chooseEmployeeAllInfo.SecondName;
+                TextBox_LastName.Text = chooseEmployeeAllInfo.SecondName;
                 TextBox_SecondName.Text = chooseEmployeeAllInfo.LastName;
                 TextBox_BirthDate.Text = chooseEmployeeAllInfo.BirthDate.ToString();
                 TextBox_Email.Text = chooseEmployeeAllInfo.Email;
