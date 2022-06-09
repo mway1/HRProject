235e71de32b6f1b2cf62b4a811944f877343d707
Maxim Kuzmenko <mway2914@yandex.ru>
2022-06-09 16:56:38 +0300

    add Employee_PositionModel for BLL

---
+++ HRproject/HRProject.BLL/OutputModels/Employee_PostionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.BLL.OutputModels
+{
+    public class Employee_PostionModel
+    {
+        public int id { get; set; }
+        public int EmployeeId { get; set; }
+        public int PositionId { get; set; }
+        public int LevelOfPositionID { get; set; }
+    }
+}
