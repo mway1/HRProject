16098f1896cfe3eaad428cdb48967c2bfa6b919a
Maxim Kuzmenko <mway2914@yandex.ru>
2022-06-09 16:07:37 +0300

    add stored procedure Employee_Position_Add

---
+++ HRproject/HRproject.DB/dbo/Stored Procedures/Employee_Position_Add.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[Employee_Position_Add]
+	@id int,
+	@EmployeeId int,
+	@PositionId int,
+	@LevelOfPositionId int
+AS
+BEGIN
+INSERT INTO dbo.Employee_Position(
+        EmployeeId,
+		PositionId,
+		LevelOfPositionId)
+VALUES(
+		@EmployeeId,
+		@PositionId,
+		@LevelOfPositionId)
+SET @id=SCOPE_IDENTITY()
+
+SELECT
+		EmployeeId=@EmployeeId,
+		PositionId=@PositionId,
+		LevelOfPositionId=@LevelOfPositionId
+FROM dbo.Employee_Position
+END
