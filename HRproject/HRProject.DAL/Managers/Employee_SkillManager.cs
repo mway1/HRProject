160d72808c7c7bcd2800a91ef571ada2992b45a1
Maxim Kuzmenko <mway2914@yandex.ru>
2022-06-10 01:31:18 +0300

    add all stored procedures and DAL for Employee_Skill

---
+++ HRproject/HRProject.DAL/Managers/Employee_SkillManager.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using HRProject.DAL.DTOs;
+using HRProject.DAL.StoredProcedureStorage;
+using System.Data.SqlClient;
+
+namespace HRProject.DAL.Managers
+{
+    public class Employee_SkillManager
+    {
+        public void AddEmployee_Skill(Employee_SkillDTO newEmployee_Skill)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle<Employee_SkillDTO>(
+                    Employee_SkillStoredProcedures.Employee_Skill_Add,
+                    param: new
+                    {
+                        newEmployee_Skill.EmployeeId,
+                        newEmployee_Skill.SkillId,
+                        newEmployee_Skill.LevelOfSkill
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+        public void DeleteEmployee_Skill(Employee_SkillDTO input)
+
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+                connection.QuerySingle(
+                   Employee_SkillStoredProcedures.Employee_Skill_Delete,
+                    param: new
+                    {
+                        input.id,
+                        input.EmployeeId,
+                        input.SkillId,
+                        input.LevelOfSkill
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+        public Employee_SkillDTO GetEmployee_SkillById(int employeeId)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<Employee_SkillDTO>(
+                    Employee_SkillStoredProcedures.Employee_Skill_GetById,
+                    param: new { EmployeeId = employeeId },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+    }
+}
---
+++ HRproject/HRproject.DB/dbo/Stored Procedures/Employee_Skill_GetById.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[Employee_Skill_GetById]
+	@EmployeeId int
+AS
+BEGIN
+
+	SELECT ES.id,ES.EmployeeId,S.Name as SkillName,ES.LevelOfSkill
+	FROM dbo.Employee_Skill as ES
+	LEFT JOIN dbo.Skill as S on (ES.SkillId=S.id)
+	
+	WHERE EmployeeId=@EmployeeId
+
+END
