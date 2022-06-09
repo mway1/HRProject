c6e5c57c956104d88b415bcc44bf31d15772a9cd
Maxim Kuzmenko <mway2914@yandex.ru>
2022-06-09 17:11:10 +0300

    add Employee_PositionManager in DAL

---
+++ HRproject/HRProject.DAL/Managers/Employee_PositionManager.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using HRProject.DAL.DTOs;
+using HRProject.DAL.StoredProcedureStorage;
+using System.Data.SqlClient;
+
+namespace HRProject.DAL.Managers
+{
+    public class Employee_PositionManager
+    {
+        public void AddEmployee(Employee_PositionDTO newEmployee_Position)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle<EmployeeDTO>(
+                    Employee_PositionStoredProcedures.Employee_Position_Add,
+                    param: new
+                    {
+                        newEmployee_Position.EmployeeId,
+                        newEmployee_Position.PositionId,
+                        newEmployee_Position.LevelOfPositionID
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+    }
+}
