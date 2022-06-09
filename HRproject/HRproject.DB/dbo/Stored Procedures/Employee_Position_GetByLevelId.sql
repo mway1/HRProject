e17245861b5feb797aaaf8466a09da931dcd2e05
Maxim Kuzmenko <mway2914@yandex.ru>
2022-06-10 00:41:49 +0300

    add stored procedure GetEmployee_Position_ByLevelId

---
+++ HRproject/HRproject.DB/dbo/Stored Procedures/Employee_Position_GetByLevelId.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Employee_Position_GetByLevelId]
+@LevelOfPositionID int
+AS
+BEGIN
+
+	SELECT EPos.id,EPos.EmployeeId,PositionId,LP.Name as [LevelOfPositionName]
+	FROM dbo.Employee_Position as EPos
+	LEFT JOIN dbo.LevelOfPosition as LP on(EPos.LevelOfPositionID=LP.id)
+	WHERE LevelOfPositionID=@LevelOfPositionID
+
+END
