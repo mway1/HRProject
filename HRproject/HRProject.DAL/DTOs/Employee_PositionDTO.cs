628d1f99c3cc1f1a460604acff471847de3c297f
Maxim Kuzmenko <mway2914@yandex.ru>
2022-06-09 16:53:32 +0300

    end DAL for Employee_Position

---
+++ HRproject/HRProject.DAL/DTOs/Employee_PositionDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.DAL.DTOs
+{
+    public class Employee_PositionDTO
+    {
+        public int id { get; set; }
+        public int EmployeeId { get; set; }
+        public int PositionId { get; set; }
+        public int LevelOfPositionID { get; set; }
+    }
+}
